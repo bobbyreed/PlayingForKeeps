478ac0523faddfc3ae07bb41fa10c08bb0b8e39f
Bobby Reed <37113385+bobbyreed@users.noreply.github.com>
2024-02-09 22:26:26 -0600

    Working towards poc
    #2 closed
    - Created base classes for levels and coins
    - Created sample  extension classes for Luke's Level and a Quarter
    
    # Next Steps
    - Need to continue working on the coin scaling when you grab and release. The logic is all their but the trigger isn't working for isHeld(). :(

---
+++ Assets/Scripts/Coins/Quarter.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Quarter : Coin
+{
+    public Transform scale;
+    private Transform bigScale;
+    private Transform smallScale;
+    private XRGrabInteractable xRGrabInteractable;
+    private bool isHeld = false;
+    private bool isBig = true;
+    private bool isSmall = false;
+    public float changeRate = .1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scale.localScale = new UnityEngine.Vector3(1, 1, 1);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(isHeld)
+      {
+        if(!isSmall)
+        {
+            Shrink();
+        }
+      }
+      else
+      {
+        if(!isBig)
+        {
+            Grow();
+        }
+      }
+      gameObject.transform.localScale = scale.localScale;
+    }
+
+    public void Grow()
+    {
+       if(gameObject.transform.localScale != bigScale.localScale)
+       {
+        Debug.Log("Growing");
+        scale.localScale += new UnityEngine.Vector3(changeRate,changeRate,changeRate);
+       }
+       else
+       {
+            isBig = true;
+       }
+        
+    }
+    public void Shrink()
+    {
+        if(gameObject.transform.localScale != smallScale.localScale)
+        {
+            Debug.Log("Shrinking");
+            scale.localScale -= new UnityEngine.Vector3(changeRate,changeRate,changeRate);
+        }
+        else
+        {
+            isSmall = true;
+        }
+        
+    }
+
+    public void IsHeldSwitch()
+    {
+        if(isHeld){
+            isHeld= false;
+        }
+        else{
+            isHeld = true;
+        }
+    }
+}
